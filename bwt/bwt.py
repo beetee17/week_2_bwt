3049ff3b8591fa9886448231c4be78dca2ad34d1
beetee17 <brandon.thio@yahoo.com.sg>
2021-05-11 15:00:37 +0800

    init

--- bwt/bwt.py
+++ bwt/bwt.py
@@ -2,7 +2,18 @@
 import sys
 
 def BWT(text):
-    return ""
+
+    M = []
+
+    for i in range(len(text)):
+        temp = text[i:] + text[:i]
+        M.append(temp)
+        
+    M.sort()
+
+    return ''.join([item[-1] for item in M])
+
+
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
--- bwtinverse/bwtinverse.py
+++ bwtinverse/bwtinverse.py
@@ -1,11 +1,49 @@
 # python3
 import sys
-
+from copy import deepcopy
 def InverseBWT(bwt):
     # write your code here
-    return ""
+
+    M = [[] for i in range(len(bwt))]
+    bwt = [[bwt[i]] for i in range(len(bwt))]
+
+    for i in range(len(bwt)):
+        
+        bwt_copy = deepcopy(bwt)      
+
+        for j in range(len(bwt)):
+
+            bwt_copy[j].extend(M[j])
+        
+        
+        M = bwt_copy
+        print(M)
+
+        M.sort()  
+           
+    return ''.join(M[0][1:]) + '$'
+
+def InverseBWT_fast(bwt):
+    # write your code here
+
+    
+    last = range(len(bwt))
+    first = sorted(range(len(bwt)), key=lambda i: bwt[i])
+
+    first_to_last = {first[i] : last[i] for i in range(len(bwt))}
+
+    index = first[0]
+    res = ''
+    
+    for i in range(len(bwt)):
+        index = first_to_last[index]
+        res += bwt[index]
+
+    return res[-2::-1] + '$'
+
+
 
 
 if __name__ == '__main__':
     bwt = sys.stdin.readline().strip()
-    print(InverseBWT(bwt))
+    print(InverseBWT_fast(bwt))
