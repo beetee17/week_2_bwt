0d4b90290e114f4f495e8db347e2b6a41ca586cd
beetee17 <brandon.thio@yahoo.com.sg>
2021-05-11 15:30:31 +0800

    fixed TLE, need to add comments

--- bwmatching/bwmatching.py
+++ bwmatching/bwmatching.py
@@ -1,8 +1,11 @@
 # python3
 import sys
 
+
+
 def PreprocessBWT(bwt):
-  """
+
+    """
   Preprocess the Burrows-Wheeler Transform bwt of some text
   and compute as a result:
     * starts - for each character C in bwt, starts[C] is the first position 
@@ -13,26 +16,42 @@ def PreprocessBWT(bwt):
         from position 0 to position P inclusive.
   """
 
-  occ_counts_before = dict()
+  occurence_dict = dict()
 
   for i in range(len(bwt)):
 
     char = bwt[i]
 
-    if not occ_counts_before.get(char, None):
+    if not occurence_dict.get(char, None):
 
-      occ_counts_before.update({ char : [0 for j in range(len(bwt)+1)] })
-
-    for index in range(len(bwt), 0, -1):
-
-      if i <= index:
-
-        occ_counts_before[char][index] += 1
-
-      else:
-
-        break
+      occurence_dict.update({ char : [] })
     
+    occurence_dict[char].append(i)
+
+  occ_counts_before = { char : [] for char in list(occurence_dict.keys())}
+
+  for char in occurence_dict.keys():
+
+    i = 0
+    j = 0
+
+    occurences = occurence_dict[char]
+
+    for occurence in occurences:
+
+      while i < occurence:
+
+        occ_counts_before[char].append(j)
+
+        i += 1
+
+      j += 1
+    
+    while i <= len(bwt):
+
+      occ_counts_before[char].append(j)
+      i += 1
+
   chars = sorted(list(occ_counts_before.keys()))
 
   starts = {char : 0 for char in chars}
@@ -70,10 +89,6 @@ def CountOccurrences(pattern, bwt, starts, occ_counts_before):
       if char not in starts:
         return 0
 
-      # pattern = pattern[:-1]
-
-      # print(char)
-      # print(top, bottom)
       if top == 0:
         num_counts = occ_counts_before[char][bottom]
 
@@ -82,9 +97,7 @@ def CountOccurrences(pattern, bwt, starts, occ_counts_before):
         num_counts = occ_counts_before[char][bottom] - occ_counts_before[char][top-1]
 
       if num_counts > 0:
-        # bottom = starts[char] + num_counts  - 1
-        # top = starts[char]
-        # print('{} occurs {} times'.format(char, num_counts))
+
         top = starts[char] + occ_counts_before[char][bottom] - num_counts
         bottom = starts[char] + occ_counts_before[char][bottom] - 1
        
@@ -93,7 +106,7 @@ def CountOccurrences(pattern, bwt, starts, occ_counts_before):
         return 0
     
     else:
-      # print('done', top, bottom)
+
       return bottom - top + 1
 
     j += 1
@@ -111,10 +124,6 @@ if __name__ == '__main__':
   # in the text instead of O(|pattern| + |text|).  
 
   starts, occ_counts_before = PreprocessBWT(bwt)
-
-  # print(starts)
-  # for char, index in occ_counts_before.items():
-  #   print(char, occ_counts_before[char], '\n')
 
   occurrence_counts = []
 
